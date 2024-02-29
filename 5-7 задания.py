b97504844b3e77f95f1855dc8cb1a725323d88fa
levvvdd <158448027+levvvdd@users.noreply.github.com>
2024-02-29 12:00:19 +0300

    5-7 задания 27.02.24

---
+++ 5-7 задания.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+a = int(input("Введите a = "))
+b = int(input("Введите b = "))
+c = (a**2 + b**2)*0.5
+print (c)
+
+a = int(input("Введите a ="))
+b = int(input("Введите b = "))
+c = (a**2 + b**2)
+P = a + b + c
+print (P)
+
+a = int(input("Введите a = "))
+b = int(input("Введите b = "))
+P = 2* (a+b)
+c = (a**2 + b**2)
+print (P,c)

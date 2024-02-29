86d4225a9e1479304f3b81cbf97b05717691face
levvvdd <158448027+levvvdd@users.noreply.github.com>
2024-02-29 20:49:23 +0300

    задание 8-15 27.02.24

---
+++ Задание 8-15.py
@@ -0,0 +1,49 @@
+#coding: utf-8
+r = int(input("Введите r ="))
+R = int(input("Введите R ="))
+p = 3.14
+S = p*(R+r)**2
+print (S)
+
+a = int(input("Введите a ="))
+S = 6*a**2
+V = a**3
+print (S,V)
+
+a = int(input("Введите a ="))
+b = int(input("Введите b ="))
+h = int(input("Введите h ="))
+P = a + b + 2 * h
+print (P)
+
+m = int(input("Введите m ="))
+V = int(input("Введите V ="))
+p=m/V
+print (p)
+
+a = int(input("Введите a ="))
+b = int(input("Введите b ="))
+сумма = a+b
+разность = a-b
+произведение = a*b
+деление = a/b
+print (сумма,разность,произведение,деление)
+
+a = int(input("Введите a ="))
+b = int(input("Введите b ="))
+avg_arithmetic = (a + b) / 2 
+avg_geometric = sqrt(a*b)
+print("Среднее арифметическое:", avg_arithmetic,"Среднее геометрическое:", avg_geometric)
+
+K = int(input("Введите K ="))
+S = int(input("Введите S ="))
+p=K/S
+print (p)
+
+a = int(input("Введите a ="))
+b = int(input("Введите b ="))
+с = int(input("Введите с ="))
+S = 2*(ab+ac+bc)
+print (S)
+
+

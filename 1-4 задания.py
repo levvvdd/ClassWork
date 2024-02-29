b3f4b607f97ef3e66fe0c37aa715dac47262e7c8
levvvdd <158448027+levvvdd@users.noreply.github.com>
2024-02-29 11:01:54 +0300

    1-4 задания 27.02.24

---
+++ 1-4 задания.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+Задание 1
+x = int (input("введите x"))
+y = 7*x+5
+print (y)
+
+Задание 2
+w = int (input("введите w"))
+e = w*4
+print (e)
+
+Задание 3
+p = 3.14
+r = int(input("Введите r ="))
+e = (p*2)*r
+print (e)
+
+Задание 4
+p = 3.14
+d = int(input("Введите d"))
+R = d/2
+u=i*R**R
+print (u)

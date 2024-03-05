085c98d70c2c602aa341ae1c481a187be43d2547
levvvdd <158448027+levvvdd@users.noreply.github.com>
2024-03-05 09:58:54 +0300

    Задание 2
    На развет.алг. 27.02.24

---
+++ Задание 2.py
@@ -0,0 +1,7 @@
+SquareSide = int(input("Введите число ="))
+CircleRad = int(input("Введите число ="))
+CircleDiam=CircleRad*2
+if CircleDiam<=SquareSide:
+    print ("Круг впишется в квадрат")
+else:
+    print ("Круг не впишется в квадрат")

ec2866327f555957326f8e6bccfb036489b13451
levvvdd <158448027+levvvdd@users.noreply.github.com>
2024-03-07 11:33:47 +0300

    Задание 4-6 07.03.24

---
+++ Задание 4-6.py
@@ -0,0 +1,19 @@
+#coding: utf-8
+#Задание №4
+N=int(input())
+k = 1
+while 3*k <= n:
+    k +=1
+print (k)
+#Задание №5
+N = int(input())
+k = n/3 + 1 
+while 3*k > n:
+    k -=1
+    print(k)
+#Задание №6
+N = int(input())
+a = 0
+while a < n:
+    a += 1 
+    print (a)

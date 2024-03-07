b75f1e9d5b2ee27c97fa4b2a3b18d57241a27162
levvvdd <158448027+levvvdd@users.noreply.github.com>
2024-03-07 11:14:57 +0300

    2 и 3 задание 07.03.24

---
+++ задание 2-3.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+#Задание №2
+a=int(input("Введите a="))
+b=int(input("Введите b="))
+for i in range(a+1,b):
+    print(i,end=" ")
+#Задание №3
+#coding: utf-8
+a=int(input("Введите a="))
+b=int(input("Введите b="))
+for i in range(b-1,a,-1):
+    print(i,end=" ")
